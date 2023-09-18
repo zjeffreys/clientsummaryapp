b3b356347c34272bcb2dbdd2a433d0ca13f7cba5
Zachary Jeffreys <91401203+zjeffreys@users.noreply.github.com>
2023-09-18 01:45:39 +0000

    open ai should work now

--- Hello.py
+++ Hello.py
@@ -1,65 +1,39 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
 import openai
 
-# Setting the API key
+# Set your OpenAI API key from Streamlit secrets
 openai.api_key = st.secrets["OPENAI_API_KEY"]
+# openai.api_key="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"
 
-if(st.secrets["OPENAI_API_KEY"] ==  "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"):
-    print("Key is correct")
-else:
-    print("Keys are not the same")
-    print(type(st.secrets["OPENAI_API_KEY"])) 
+# Create a Streamlit app
+def main():
+    st.title("Streamlit OpenAI Demo")
+    st.write("Use the OpenAI API to generate text!")
 
+    # Input text box for user prompt
+    user_prompt = st.text_area("Enter a prompt:", "Once upon a time,")
 
-# Perform tasks using OpenAI API
-# print(openai.Model.list()) # List all OpenAI models
+    if st.button("Generate Text"):
+        if user_prompt:
+            try:
+                # Use the OpenAI API to generate text
+                response = openai.Completion.create(
+                    engine="davinci",
+                    prompt=user_prompt,
+                    max_tokens=50,  # Adjust as needed
+                    n = 1,
+                    stop=None
+                )
+                generated_text = response.choices[0].text
 
-LOGGER = get_logger(__name__)
+                # Display the generated text
+                st.write("Generated Text:")
+                st.write(generated_text)
 
-
-def run():
-    st.set_page_config(
-        page_title="Client Summaries",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to The Client Summary App! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Yo ...Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
+            except Exception as e:
+                st.error(f"Error: {e}")
+        else:
+            st.warning("Please enter a prompt.")
 
 if __name__ == "__main__":
-    run()
+    main()
