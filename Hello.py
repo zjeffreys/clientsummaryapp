ca16995e4a95394183e3d9e9cf843e5f3e337a7c
Zachary Jeffreys <91401203+zjeffreys@users.noreply.github.com>
2023-09-18 03:41:30 +0000

    try to add pdf now

--- Hello.py
+++ Hello.py
@@ -1,39 +1,125 @@
 import streamlit as st
-import openai
+from streamlit_extras.add_vertical_space import add_vertical_space
+from PyPDF2 import PdfFileReader, PdfFileWriter,PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
 
-# Set your OpenAI API key from Streamlit secrets
-openai.api_key = st.secrets["OPENAI_API_KEY"]
-# openai.api_key="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"
+import pickle
+import os
+#load api key lib
+from dotenv import load_dotenv
+import base64
 
-# Create a Streamlit app
+
+#Background images add function
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"jpeg"};base64,{encoded_string.decode()});
+        background-size: cover;
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+#add_bg_from_local('images.jpeg')  
+
+#sidebar contents
+
+with st.sidebar:
+    st.title('🦜️🔗VK - PDF BASED LLM-LANGCHAIN CHATBOT🤗')
+    st.markdown('''
+    ## About APP:
+
+    The app's primary resource is utilised to create::
+
+    - [streamlit](https://streamlit.io/)
+    - [Langchain](https://docs.langchain.com/docs/)
+    - [OpenAI](https://openai.com/)
+
+    ## About me:
+
+    - [Linkedin](https://www.linkedin.com/in/venkat-vk/)
+    
+    ''')
+
+    add_vertical_space(4)
+    st.write('💡All about pdf based chatbot, created by VK🤗')
+
+load_dotenv()
+
 def main():
-    st.title("Streamlit OpenAI Demo")
-    st.write("Use the OpenAI API to generate text!")
+    st.header("📄Chat with your pdf file🤗")
 
-    # Input text box for user prompt
-    user_prompt = st.text_area("Enter a prompt:", "Once upon a time,")
+    #upload a your pdf file
+    pdf = st.file_uploader("Upload your PDF", type='pdf')
+    st.write(pdf.name)
 
-    if st.button("Generate Text"):
-        if user_prompt:
-            try:
-                # Use the OpenAI API to generate text
-                response = openai.Completion.create(
-                    engine="davinci",
-                    prompt=user_prompt,
-                    max_tokens=50,  # Adjust as needed
-                    n = 1,
-                    stop=None
-                )
-                generated_text = response.choices[0].text
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
 
-                # Display the generated text
-                st.write("Generated Text:")
-                st.write(generated_text)
+        text = ""
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
 
-            except Exception as e:
-                st.error(f"Error: {e}")
+        #langchain_textspliter
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 1000,
+            chunk_overlap = 200,
+            length_function = len
+        )
+
+        chunks = text_splitter.split_text(text=text)
+
+        
+        #store pdf name
+        store_name = pdf.name[:-4]
+        
+        if os.path.exists(f"{store_name}.pkl"):
+            with open(f"{store_name}.pkl","rb") as f:
+                vectorstore = pickle.load(f)
+            #st.write("Already, Embeddings loaded from the your folder (disks)")
         else:
-            st.warning("Please enter a prompt.")
+            #embedding (Openai methods) 
+            embeddings = OpenAIEmbeddings()
 
-if __name__ == "__main__":
+            #Store the chunks part in db (vector)
+            vectorstore = FAISS.from_texts(chunks,embedding=embeddings)
+
+            with open(f"{store_name}.pkl","wb") as f:
+                pickle.dump(vectorstore,f)
+            
+            #st.write("Embedding computation completed")
+
+        #st.write(chunks)
+        
+        #Accept user questions/query
+
+        query = st.text_input("Ask questions about related your upload pdf file")
+        #st.write(query)
+
+        if query:
+            docs = vectorstore.similarity_search(query=query,k=3)
+            #st.write(docs)
+            
+            #openai rank lnv process
+            llm = OpenAI(temperature=0)
+            chain = load_qa_chain(llm=llm, chain_type= "stuff")
+            
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents = docs, question = query)
+                print(cb)
+            st.write(response)
+
+
+
+if __name__=="__main__":
     main()
