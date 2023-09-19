626dea7415f0071eb870ee9cc8be775410491760
Zachary Jeffreys <91401203+zjeffreys@users.noreply.github.com>
2023-09-19 04:09:56 +0000

    changed alot of stuff

--- Hello.py
+++ Hello.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_extras.add_vertical_space import add_vertical_space
-from PyPDF2 import PdfFileReader, PdfFileWriter,PdfReader
+from PyPDF2 import PdfFileReader, PdfFileWriter, PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
@@ -10,116 +10,137 @@ from langchain.callbacks import get_openai_callback
 
 import pickle
 import os
-#load api key lib
+# Load api key lib
 from dotenv import load_dotenv
 import base64
 
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from PIL import Image
+import io
 
-#Background images add function
+
+load_dotenv()
+
+# Background images add function
 def add_bg_from_local(image_file):
     with open(image_file, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
     st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"jpeg"};base64,{encoded_string.decode()});
-        background-size: cover;
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
+        f"""
+        <style>
+        .stApp {{
+            background-image: url(data:image/{"jpeg"};base64,{encoded_string.decode()});
+            background-size: cover;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
     )
-#add_bg_from_local('images.jpeg')  
 
-#sidebar contents
+def generate_pdf(user_text):
+    # Create a PDF buffer using StringIO
+    pdf_buffer = io.BytesIO()
 
-with st.sidebar:
-    st.title('🦜️🔗VK - PDF BASED LLM-LANGCHAIN CHATBOT🤗')
-    st.markdown('''
-    ## About APP:
+    # Create a new PDF document
+    c = canvas.Canvas(pdf_buffer, pagesize=letter)
 
-    The app's primary resource is utilised to create::
+    # Add user text to the PDF
+    c.drawString(50, 600, user_text)
 
-    - [streamlit](https://streamlit.io/)
-    - [Langchain](https://docs.langchain.com/docs/)
-    - [OpenAI](https://openai.com/)
+    # Save the PDF to the buffer
+    c.save()
 
-    ## About me:
-
-    - [Linkedin](https://www.linkedin.com/in/venkat-vk/)
-    
-    ''')
-
-    add_vertical_space(4)
-    st.write('💡All about pdf based chatbot, created by VK🤗')
-
-load_dotenv()
+    # Reset the buffer position
+    pdf_buffer.seek(0)
+    return pdf_buffer
 
 def main():
-    st.header("📄Chat with your pdf file🤗")
+    st.header("📄Summarize Insurance Docs 🤗")
 
-    #upload a your pdf file
-    pdf = st.file_uploader("Upload your PDF", type='pdf')
-    st.write(pdf.name)
+    # upload a your pdf file
+    pdf = st.file_uploader("Step 1 - Upload your PDF", type='pdf')
+   
 
     if pdf is not None:
+        # st.write(pdf.name)
         pdf_reader = PdfReader(pdf)
 
         text = ""
         for page in pdf_reader.pages:
-            text+= page.extract_text()
+            text += page.extract_text()
 
-        #langchain_textspliter
+        # langchain_textspliter
         text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size = 1000,
-            chunk_overlap = 200,
-            length_function = len
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
         )
 
         chunks = text_splitter.split_text(text=text)
 
-        
-        #store pdf name
+        # store pdf name
         store_name = pdf.name[:-4]
-        
+
         if os.path.exists(f"{store_name}.pkl"):
-            with open(f"{store_name}.pkl","rb") as f:
+            with open(f"{store_name}.pkl", "rb") as f:
                 vectorstore = pickle.load(f)
-            #st.write("Already, Embeddings loaded from the your folder (disks)")
+            # st.write("Already, Embeddings loaded from the your folder (disks)")
         else:
-            #embedding (Openai methods) 
+            # embedding (Openai methods)
             embeddings = OpenAIEmbeddings()
 
-            #Store the chunks part in db (vector)
-            vectorstore = FAISS.from_texts(chunks,embedding=embeddings)
+            # Store the chunks part in db (vector)
+            vectorstore = FAISS.from_texts(chunks, embedding=embeddings)
 
-            with open(f"{store_name}.pkl","wb") as f:
-                pickle.dump(vectorstore,f)
+            with open(f"{store_name}.pkl", "wb") as f:
+                pickle.dump(vectorstore, f)
+
+            # st.write("Embedding computation completed")
+
+        # st.write(chunks)
+
+        st.markdown("""---""")
+        # Accept user questions/query
+        left_column, right_column = st.columns(2)
+
+        with left_column:
+            query = st.text_input("Step 2 - Ask questions about the information you need.", "Please summarize this policy")
+            query = query + 'Add new line characters in the proper places'
+            query = query + "Always provide page numbers to reference where you found the information"
+            if query:
+                docs = vectorstore.similarity_search(query=query, k=3)
+                # openai rank lnv process
+                llm = OpenAI(temperature=0)
+                chain = load_qa_chain(llm=llm, chain_type="stuff")
+
+                with get_openai_callback() as cb:
+                    response = chain.run(input_documents=docs, question=query)
+                    print(cb)
+                st.write(":blue[AI Response]")
+                st.markdown(response)                
+        with right_column:
+            st.title("PDF Generator")
+
+            # User inputs
+            user_text = st.text_area("Step 3) Copy Information To Here", "This will be used to create a printable document.")
+
+            if st.button("Generate PDF"):
+                if user_text:
+                    # Generate the PDF
+                    pdf_buffer = generate_pdf(user_text)
+                    if pdf_buffer:
+                        # Offer the PDF download
+                        st.download_button("Download PDF", pdf_buffer, file_name="Insurace_Quote.pdf", key="pdf-download")
+                else:
+                    st.error("Please enter some text.")
+
             
-            #st.write("Embedding computation completed")
 
-        #st.write(chunks)
-        
-        #Accept user questions/query
-
-        query = st.text_input("Ask questions about related your upload pdf file")
-        #st.write(query)
-
-        if query:
-            docs = vectorstore.similarity_search(query=query,k=3)
-            #st.write(docs)
             
-            #openai rank lnv process
-            llm = OpenAI(temperature=0)
-            chain = load_qa_chain(llm=llm, chain_type= "stuff")
-            
-            with get_openai_callback() as cb:
-                response = chain.run(input_documents = docs, question = query)
-                print(cb)
-            st.write(response)
+
+     
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -18,11 +18,11 @@ import textwrap
 import streamlit as st
 
 
-def show_code(demo):
-    """Showing the code of the demo."""
-    show_code = st.sidebar.checkbox("Show code", True)
-    if show_code:
-        # Showing the code of the demo.
-        st.markdown("## Code")
-        sourcelines, _ = inspect.getsourcelines(demo)
-        st.code(textwrap.dedent("".join(sourcelines[1:])))
+# def show_code(demo):
+#     """Showing the code of the demo."""
+#     show_code = st.sidebar.checkbox("Show code", True)
+#     if show_code:
+#         # Showing the code of the demo.
+#         st.markdown("## Code")
+#         sourcelines, _ = inspect.getsourcelines(demo)
+#         st.code(textwrap.dedent("".join(sourcelines[1:])))
